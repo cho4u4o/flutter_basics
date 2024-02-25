2438acbb82ac28a44a8361b484dd88e544c5280e
cho4u4o <cho4u4o@gmail.com>
2024-02-26 00:19:10 +0900

    240226 walletapp(1)

--- wallet_app/lib/main.dart
+++ wallet_app/lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,11 +10,39 @@ class App extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.red.shade800,
-        body: Center(
-          child: Text('Hello?'),
-        ),
+        backgroundColor: Color(0xFF181818),
+        body: Column(
+          children: [
+            SizedBox(
+              height: 50,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text('Hey, Selena',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 34,
+                        fontWeight: FontWeight.w800,
+                      ),
+                    ),
+                    Text('Welcome back',
+                      style: TextStyle(
+                        color: Colors.white.withOpacity(0.8),
+                        fontSize: 18,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            )
+          ],
+        )
       ),
     );
   }

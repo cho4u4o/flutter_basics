ae08725e20949246c53078957a5e965706e7494b
cho4u4o <cho4u4o@gmail.com>
2024-04-02 14:02:56 +0900

    240402 all recap; app build

---
+++ movie_app/lib/services/api_service.dart
@@ -0,0 +1,9 @@
+import 'package:movie_app/models/artist.dart';
+
+class ApiService {
+  static const String baseURL = "https://api.spotify.com/v1/artists/";
+  static const String drake = "3TVXtAsR1Inumwj472S9r4";
+
+  static Future<List<ArtistModel>> getArtist() async {
+    List<ArtistModel>;
+}
